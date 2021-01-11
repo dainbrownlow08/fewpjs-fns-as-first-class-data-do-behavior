0e81a097afddd9fc3b39a2e1e7a95f36434a6d4d
dainbrownlow08 <dain.brownlow@outlook.com>
2021-01-11 13:28:04 -0800

    commit tree df6d7561f6b81adf2c7e2b45cec8facfb57ee093

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* Given Code, don't edit */
 
-document.querySelector("button").addEventListener("click", handleClick);
+// document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
   const timeString = document.getElementById('time').value
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(tS){
+  var hour = parseInt(tS.split(":")[0]);
+  if(hour < 12){
+    return "Good Morning";
+  }
+  if(hour >= 12 && hour <= 17){
+    return "Good Afternoon";
+  }
+  if(hour > 17 && hour <= 24){
+    return "Good Evening";
+  }
+  throw "Invalid time entry." 
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(s){
+  document.getElementById("greeting").innerText = s;
+}
